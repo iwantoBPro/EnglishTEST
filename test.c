f75e133efd5c43935ba961a6f2db51f61a76e9c2
GeonGeon <a1163123@gmail.com>
2022-05-12 18:31:39 +0900

    .\test.c

--- test.c
+++ test.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h> 
 #include <windows.h>
+#include <stdlib.h>
 
 int main()
 {
     int num = 0;
     char trash[100];
-    char Word[100][100] = {NULL, };
-    char Mean[100][100] = {NULL, };
-    char Answer[100][100] = {NULL, };
+    char Word[100][100] = {0, };
+    
+    char Mean[100][100] = {0, };
+    char Answer[100][100] = {0, };
     
     int score = 0;
 
@@ -25,29 +27,42 @@ int main()
         gets(Mean[i]);
 
     system("cls");
+    int flag[100] = {0,};
     for(int i = 0; i < num; i++)
     {
-        printf("%d. %s     :       ", i+1 , Mean[i]);
-        gets(Answer[i]);
+        int j = 0;
 
-        int debugnum = strcmp(Answer[i], Word[i]);
-
-        if (debugnum == 0)
-        {
-            score++;
-            printf("OOO정답입니다!OOO                                 score : %d \n", score);
-            
-        }
+        a:
+        j = rand()%num;
+        
+        if(flag[j] == 1)
+            goto a;
 
         else
         {
-            printf("XXX오답입니다 메롱XXX                             score : %d  \n", score);
-            printf("사실 정답은 %s 이지롱 바보\n", Word[i], score);
-        }
+            printf("%d. %s     :       ", i+1 , Mean[j]);
+            gets(Answer[j]);
 
-        printf("다음 문제.....  Enter Please \n");
-        getchar();
-        system("cls");
+            int debugnum = strcmp(Answer[j], Word[j]);
+
+            if (debugnum == 0)
+            {
+                score++;
+                printf("OOO정답입니다!OOO                                 score : %d \n", score);
+                
+            }
+
+            else
+            {
+                printf("XXX오답입니다 메롱XXX                             score : %d  \n", score);
+                printf("사실 정답은 %s 이지롱 바보\n", Word[j], score);
+            }
+
+            flag[j] = 1;
+            printf("다음 문제.....  Enter Please \n");
+            getchar();
+            system("cls");
+        }
     }
 
     printf("당신의 점수는~~~~~~~~~~~~~~~~~~~    %d점 입니다!   \n", score);
